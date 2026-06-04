4914842927ff8de7a64686beedef15e99856c335
Ivan Diego Guimarães Lopes <ivandglopes@hotmail.com>
2026-06-04 18:14:18 -0300

    Aprimorado de concatenação para fstring

--- report.py
+++ report.py
@@ -1,9 +1,9 @@
 name = input("Write your full name: ")
 age = input("How old are you? ")
-score = input("How much do you get in the exam? ")
+score = input("How much do you get on the exam? ")
 
-print("Hi,", name,".")
-print(name, "you has", age, "years old and get", score, "in the exam.")
+print(f"Hi, {name}.")
+print(f"{name}, you are {age} years old and get {score} in the exam.")
 
 if int(score) >= 70:
     print("You are approved!")
